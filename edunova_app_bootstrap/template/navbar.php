2221c2941213a2b58f505e178baa48cd9f302fc8
Danijel Sugar <danijel.sugar@outlook.com>
2018-06-30 20:00:49 +0200

    First commit: Navbar

---
+++ edunova_app_bootstrap/template/navbar.php
@@ -0,0 +1,25 @@
+<div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="<?php echo  $putanjaAPP; ?>index.php"><i class="fas fa-home"></i></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $putanjaAPP; ?>privatno/dashboard.php">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $putanjaAPP; ?>about-us.php">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $putanjaAPP; ?>contact.php">Contact</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mr-right">
+        <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
