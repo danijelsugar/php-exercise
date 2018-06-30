5a2542eed8c2717084ba212e3fb14f04386ba262
Danijel Sugar <danijel.sugar@outlook.com>
2018-06-30 20:00:35 +0200

    First commit: Heading

---
+++ edunova_app_bootstrap/template/heading.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="col col-lg-12">
+		<div class="row">
+			<div class="col col-lg-6">Logo</div>
+			<div class="col col-lg-6">Header</div>
+		</div>
+	</div>
+</div>
