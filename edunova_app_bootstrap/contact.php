3328ed05c75c3bbd3d66633d2ed94fc133fe333d
Danijel Sugar <danijel.sugar@outlook.com>
2018-06-30 19:58:26 +0200

    First commit: Contact

---
+++ edunova_app_bootstrap/contact.php
@@ -0,0 +1,20 @@
+<?php include_once "configuration.php" ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include_once "template/head.php" ?>
+</head>
+<body>
+	
+	<?php include_once "template/heading.php" ?>
+
+	<?php include_once "template/navbar.php" ?>
+	<div class="container">
+		Contact
+	</div>
+	<?php include_once "template/scripts.php" ?>  
+
+	<?php include_once "template/footer.php" ?> 
+	
+</body>
+</html>
