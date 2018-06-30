d45dbe483f787d78a1f2f93d8e66c23ddd666fed
Danijel Sugar <danijel.sugar@outlook.com>
2018-06-30 20:00:59 +0200

    First commit: Scripts

---
+++ edunova_app_bootstrap/template/scripts.php
@@ -0,0 +1,6 @@
+ <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="<?php echo  $putanjaAPP; ?>js/jquery.js"></script>
+  <script src="<?php echo  $putanjaAPP; ?>js/bootstrap.bundle.min.js"></script>
+  
+  
