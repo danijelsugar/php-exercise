fc01f15995bece46de07b45f68246c88d7cf0462
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-09 16:46:10 +0200

    Changed navbar to show class active if $putanjaAPP and $stranica is equal to ip in addres bar

--- edunova_app_bootstrap/template/navbar.php
+++ edunova_app_bootstrap/template/navbar.php
@@ -1,25 +1,30 @@
 <div class="container">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="<?php echo  $putanjaAPP; ?>index.php"><i class="fas fa-home"></i></a>
+    <a  class="navbar-brand <?php if($putanjaAPP . 'index.php' === $_SERVER['PHP_SELF']) echo 'active'; ?>" href="<?php echo  $putanjaAPP; ?>index.php"><i class="fas fa-home"></i></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo $putanjaAPP; ?>privatno/dashboard.php">Dashboard</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo $putanjaAPP; ?>about-us.php">About us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo $putanjaAPP; ?>contact.php">Contact</a>
-        </li>
+      <?php 
+      if(isset($_SESSION[$idAPP."o"])):
+      stavkaIzbornika($putanjaAPP, "privatno/dashboard.php", "Dashboard");
+      stavkaIzbornika($putanjaAPP, "privatno/PDO.php", "PDO");
+      endif;
+      stavkaIzbornika($putanjaAPP, "about-us.php", "About Us");
+      stavkaIzbornika($putanjaAPP, "contact.php", "Contact");
+
+      ?>
       </ul>
       <ul class="navbar-nav mr-right">
-        <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+
+        <?php if(isset($_SESSION[$idAPP."o"])): ?>
+          <li class="nav-item"><a class="nav-link" href="<?php echo $putanjaAPP; ?>logout.php">Logout</a></li>
+        <?php else: ?>
+          <li class="nav-item"><a class="nav-link" href="<?php echo $putanjaAPP; ?>login.php">Login</a></li>
+        <?php endif; ?>
       </ul>
-    </div>
-  </nav>
-</div>
+      </div>
+    </nav>
+  </div>
