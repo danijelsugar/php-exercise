16aeb76148975fffc5d0234625e8cc6986ebb71d
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-09 16:29:36 +0200

    no message

--- edunova_app_bootstrap/template/scripts.php
+++ edunova_app_bootstrap/template/scripts.php
@@ -1,6 +1,9 @@
  <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="<?php echo  $putanjaAPP; ?>js/jquery.js"></script>
-  <script src="<?php echo  $putanjaAPP; ?>js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $putanjaAPP; ?>js/jquery.js"></script>
+  <script src="<?php echo $putanjaAPP; ?>js/bootstrap.bundle.min.js"></script>
+
+  
+  
   
   
