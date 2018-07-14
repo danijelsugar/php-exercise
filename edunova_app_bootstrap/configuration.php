6ef4e441c14931703b97ec0ab983e992f9765186
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-15 00:52:31 +0200

    Added $veza in configuration so i can include it where i need it and connect to database

--- edunova_app_bootstrap/configuration.php
+++ edunova_app_bootstrap/configuration.php
@@ -17,5 +17,10 @@
     break;
 }
 
-
+    /* Byet host */
+    //$veza = new PDO("mysql:host=sql304.byethost.com;port=3306;dbname=b16_21955356_edunovapp17","b16_21955356","bogibatina13");
+    
+    /*Local host*/
+    $veza = new PDO("mysql:host=localhost;dbname=edunovapp17","edunova","edunova");
+    $veza->exec("set names utf8;");
 ?>
